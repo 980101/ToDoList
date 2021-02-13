f6bc633a02b753ee3b75c19c8b6724b29cad49f3
hj <hyejuggang@gmail.com>
2021-02-14 00:54:58 +0900

    UPDATE style

--- clock.js
+++ clock.js
@@ -9,7 +9,8 @@ function getTime() {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${
+    clockTitle.innerHTML = `${
+        hours < 10 ? `0${hours}` :hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
         seconds < 10 ? `0${seconds}` : seconds}`;
 }
