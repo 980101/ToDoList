d78098d5ea9b0825c7dfb87a52b63c24a364f5b8
980101 <jm30806@naver.com>
2021-07-18 15:47:59 +0900

    ADD tags

--- clock.js
+++ clock.js
@@ -9,8 +9,7 @@ function getTime() {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${
-        hours < 10 ? `0${hours}` :hours}:${
+    clockTitle.innerHTML = `${hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
         seconds < 10 ? `0${seconds}` : seconds}`;
 }
