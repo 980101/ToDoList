5b6c0e2672dac0723b844206e369e88f2222956e
980101 <jm30806@naver.com>
2021-07-23 23:44:16 +0900

    '시계' 디자인 변경

--- clock.js
+++ clock.js
@@ -1,17 +1,17 @@
-/*const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");*/
-
-const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+const clockSecond = document.querySelector(".second");
+const clockState = document.querySelector(".clock");
 
 function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${
-        seconds < 10 ? `0${seconds}` : seconds}`;
+    clockTitle.innerHTML = `${hours < 12 ? hours : `${hours - 12}`}:${minutes < 10 ? `0${minutes}` : minutes}`;
+    clockSecond.innerHTML = `${seconds < 10 ? `0${seconds}` : seconds}`;
+    clockState.innerHTML = hours < 12 ? "AM" : "PM";
+
 }
 
 function init() {
